00221ead679fa044084fd8299bbef3c511cd86e6
alexander.khilko <alexkhilko92@gmail.com>
2020-03-02 12:39:33 +0300

    Add login vue component

---
+++ frontend/src/components/Login.vue
@@ -0,0 +1,32 @@
+<template>
+ <div>
+   <form class="login" @submit.prevent="login">
+     <h1>Войдите</h1>
+     <label>Почтовый ящик</label>
+     <input required v-model="email" type="text" placeholder="Name"/>
+     <label>Пароль</label>
+     <input required v-model="password" type="password" placeholder="Password"/>
+     <hr/>
+     <button type="submit">Войти</button>
+   </form>
+ </div>
+</template>
+<script>
+	export default {
+		data(){
+			return {
+				email : "",
+            	password : ""
+            }
+		},
+		methods: {
+		 	login: function () {
+		   		let email = this.email 
+		   		let password = this.password
+		   		this.$store.dispatch('login', { email, password })
+		   		.then(() => this.$router.push('/'))
+		   		.catch(err => console.log(err))
+		   	}
+		}
+	}
+</script>
