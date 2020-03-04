1bd79fac1f3181a4d68d1a3642b357a2db284662
alexander.khilko <alexkhilko92@gmail.com>
2020-03-04 11:05:52 +0300

    Modified base api url to use https

--- frontend/config/prod.env.js
+++ frontend/config/prod.env.js
@@ -1,5 +1,5 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  VUE_APP_API_URL: JSON.stringify(`http://${process.env.BASE_URL}/api`)
+  VUE_APP_API_URL: JSON.stringify(`https://${process.env.BASE_URL}/api`)
 }
